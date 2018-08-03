e7e654d0a006bb496861bf9453072eda80cb18c4
Michael Smith <msmithboise@gmail.com>
2018-08-03 15:39:58 -0600

    push more changes

--- game.js
+++ game.js
@@ -1,18 +1,47 @@
 const Squirtle = {
     name: 'Squirtle',
-    hp: 100
+    hp: 100,
+    hits: 0
 }
 
 
 
+
+function hits(){
+    Squirtle.hits ++;
+}  
 
 
 function slap(){
  Squirtle.hp -= 1
- alert(Squirtle.hp)
+ Squirtle.hits ++;
+ update(Squirtle.hp)
+ }
+
+
+function fireball(){
+    Squirtle.hp -= 5
+    update(Squirtle.hp)
 }
+
+
+function firestorm(){
+    Squirtle.hp -=10
+    update(Squirtle.hp)
+}
+
+
+
+
+
+
+
+
+
+
 
 function update(){
     document.getElementById("health").innerHTML = Squirtle.hp;
-}
+}   document.getElementById("hits").innerHTML = Squirtle.hits;
+    document.getElementById("name").innerHTML = Squirtle.name;
 update()
