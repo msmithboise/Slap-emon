73d78d313f853ae48662789f14becda4b9e8210c
Michael Smith <msmithboise@gmail.com>
2018-08-03 15:08:59 -0600

    hp works now

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 const Squirtle = {
-name: 'Squirtle',
-hp: 100
+    name: 'Squirtle',
+    hp: 100
 }
 
 
 
-draw()
+
 
 function slap(){
  Squirtle.hp -= 1
  alert(Squirtle.hp)
 }
 
-
-slap()
+function update(){
+    document.getElementById("health").innerHTML = Squirtle.hp;
+}
+update()
